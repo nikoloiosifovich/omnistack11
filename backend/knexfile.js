6e2d06954ff4d72b62429ac2d6eff8df7b6f85f3
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-24 16:56:57 -0300

    Setting: make migrations for tables onds&incidents

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -6,7 +6,11 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './src/database/db.sqlite'
-    }
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
   },
 
   staging: {
