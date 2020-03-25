0d25c1380b7f85e26892b99644f8cea932a5c211
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 17:53:35 -0300

    Implement: usability of stats

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
-// JSX ( JavaScript + XML )
-// Componentes = Conjunto de funções
+/**
+ * JSX ( JavaScript + XML )
+ * Componentes = Conjunto de funções
+ * O React não usa variáveis convensionais
+ * ele usa Estado para refletir as atualizações
+ * de na aplicação
+ */
 
 function App() {
-  return (
-    <Header>
-      Semana Omnistack
-    </Header>
+  // useState -> Array [ valor, funcaoDeAtualizacao ]
+  // no React há o principio da imutabilidade
+  const [ counter, setCounter ] = useState( 0 );
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return ( // componentes devem ficar dentro de algo no react
+    <div>
+      <Header>
+        Contador: { counter }
+      </Header>
+      <button onClick={ increment }>Incrementar</button>
+    </div>
   );
 }
 
