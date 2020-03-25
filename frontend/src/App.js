44d45a44ba0a20200e126a04b3fe2f3a422ca32a
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 19:49:28 -0300

    Implement: struct of Logon page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Logon from './pages/Logon';
 
 /**
  * JSX ( JavaScript + XML )
@@ -8,24 +10,16 @@ import Header from './Header';
  * O React não usa variáveis convensionais
  * ele usa Estado para refletir as atualizações
  * de na aplicação
+ *
+ * useState -> Array [ valor, funcaoDeAtualizacao ]
+ * no React há o principio da imutabilidade
+ *
+ * componentes devem ficar dentro de algo no react
  */
 
 function App() {
-  // useState -> Array [ valor, funcaoDeAtualizacao ]
-  // no React há o principio da imutabilidade
-  const [ counter, setCounter ] = useState( 0 );
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
-  return ( // componentes devem ficar dentro de algo no react
-    <div>
-      <Header>
-        Contador: { counter }
-      </Header>
-      <button onClick={ increment }>Incrementar</button>
-    </div>
+  return (
+    <Logon />
   );
 }
 
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png';
+
+export default function Logon() {
+  return(
+
+    <div className="logon-container">
+
+      <section className="form">
+        <img src={ logoImg } alt="Logo Be the Hero"/>
+
+        <form action="">
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" />
+          <button type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={ 16 } color="#e02041" />
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+
+      <img src={ heroesImg } alt="Heroes" />
+
+    </div>
+
+  );
+}
