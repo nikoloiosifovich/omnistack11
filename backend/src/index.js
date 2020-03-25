5a839f6218910f45db4b9c97727dfc935be18964
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-24 21:45:28 -0300

    Settin: cors for access control

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require( 'express' );
+const cors = require( 'cors' );
 const routes = require( './routes' );
 
 const app = express();
+
+app.use( cors() ); // { origin: 'url' }
 app.use( express.json() );
 app.use( routes );
 
