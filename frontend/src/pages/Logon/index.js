1da8ec17a7c33eb24156a02dd731c0a1937e96bb
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 20:59:30 -0300

    Change: implement Link on Logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -20,10 +21,10 @@ export default function Logon() {
           <input placeholder="Sua ID" />
           <button className="button" type="submit">Entrar</button>
 
-          <a href="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={ 16 } color="#e02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
 
