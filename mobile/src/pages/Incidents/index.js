b806635b421aae582ee89a34c28bcfb0ee256403
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 18:05:58 -0300

    Implement: header of incidents pages

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Incidents() {
   return (
-    <View />
+    <View style={ styles.container }>
+      <View style={ styles.header }>
+
+        <Image source={ logoImg } />
+        <Text style={ styles.headerText }>
+          Total de <Text style={ styles.headerTextBold }>0 casos</Text>.
+        </Text>
+
+        <Text style={ styles.title }>Bem-Vindo!</Text>
+        <Text style={ styles.description }>Escolha um dos casos abaixo e salve o dia.</Text>
+
+      </View>
+    </View>
   );
 }
