0a848e49ae6e03d942a75fff02032a3ead0eb99c
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 17:43:50 -0300

    Implement: start routes file

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View>
-      <Text>!!Open up App.js to start working on your app!</Text>
-    </View>
+    <Routes />
   );
 }
