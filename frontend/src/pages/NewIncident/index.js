19a8ab737734f7421ab6dcf3f6a62bc22b78ff71
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 22:18:02 -0300

    Setting: routes and basic files for NewIncident page

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,9 @@
+import React from 'react' ;
+import { Link } from 'react-router-dom';
+import {} from 'react-icons';
+
+import './styles.css';
+
+export default function NewIncident() {
+  return <h1>NewIncident</h1>
+}
