b28e959d085c39c9c44b3c5787c4e86f111c0b64
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 22:06:45 -0300

    Implement: struct of content for Profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { FiPower } from 'react-icons/fi'
+import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import './styles.css';
 
@@ -20,6 +20,70 @@ export default function Profile() {
           <FiPower size={ 18 } color="#e02041" />
         </button>
       </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição do caso</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 1.200,00</p>
+
+          <button type="button">
+            <FiTrash2 size={ 20 } color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição do caso</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 1.200,00</p>
+
+          <button type="button">
+            <FiTrash2 size={ 20 } color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição do caso</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 1.200,00</p>
+
+          <button type="button">
+            <FiTrash2 size={ 20 } color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição do caso</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 1.200,00</p>
+
+          <button type="button">
+            <FiTrash2 size={ 20 } color="#a8a8b3" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
