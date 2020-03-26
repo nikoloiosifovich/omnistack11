cd59a1d9b44d576dcd9eb18d72cd0c1683a1bc2c
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 13:54:51 -0300

    Setting: add splash and icons

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Hello World!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
