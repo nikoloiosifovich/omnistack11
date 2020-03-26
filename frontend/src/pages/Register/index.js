5f20cf7c91b672d06494d16edb5a67f8f2ca3f72
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 21:00:22 -0300

    Implement: struct for Register page

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css'
 
+import logoImg from '../../assets/logo.svg'
+
 export default function Register() {
-  return <h1>Register</h1>
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={ logoImg } alt="Logo: Be The Hero"/>
+
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.
+          </p>
+
+          <Link className="back-link" to="/register">
+            <FiArrowLeft size={ 16 } color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Nome da ONG"/>
+          <input type="email" placeholder="E-mail"/>
+          <input placeholder="WhatsApp"/>
+
+          <div className="input-group">
+            <input placeholder="Cidade"/>
+            <input placeholder="Uf" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
