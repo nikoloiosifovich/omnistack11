411064b6872caf1ce6f9426a8e865070fb071a36
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 19:06:17 -0300

    Implement: styles for incident list of Incidents page

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -31,4 +31,36 @@ export default StyleSheet.create({
     lineHeight: 24,
     color: '#737380'
   },
+
+  incidentList: {
+    marginTop: 32,
+  },
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    marginBottom: 16
+  },
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold'
+  },
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    marginBottom: 24,
+    color: '#737380'
+  },
+
+  detailsButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  detailsButtonText: {
+    color: '#e02041',
+    fontSize: 15,
+    fontWeight: 'bold'
+  },
 });
