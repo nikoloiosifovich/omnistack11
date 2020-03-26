033eb7e761ec30eb1e2af505f347894638a0f79b
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 19:05:16 -0300

    Change: refactory Incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
@@ -22,67 +22,35 @@ export default function Incidents() {
       <Text style={ styles.title }>Bem-Vindo!</Text>
       <Text style={ styles.description }>Escolha um dos casos abaixo e salve o dia.</Text>
 
-      <View style={ styles.incidentList }>
-        <View style={ styles.incident }>
+      <FlatList
+        style={ styles.incidentList }
+        data={[ 1, 2, 3 ]}
+        keyExtractor={ incident => String(incident) }
+        showsVerticalScrollIndicator={ false }
+        renderItem={ () => (
+          <View style={ styles.incident }>
 
-          <Text style={ styles.incidentProperty }>ONG:</Text>
-          <Text style={ styles.incidentValue }>APAD</Text>
+            <Text style={ styles.incidentProperty }>ONG:</Text>
+            <Text style={ styles.incidentValue }>APAD</Text>
 
-          <Text style={ styles.incidentProperty }>CASO:</Text>
-          <Text style={ styles.incidentValue }>Cadelinha atropelada</Text>
+            <Text style={ styles.incidentProperty }>CASO:</Text>
+            <Text style={ styles.incidentValue }>Cadelinha atropelada</Text>
 
-          <Text style={ styles.incidentProperty }>VALOR:</Text>
-          <Text style={ styles.incidentValue }>R$ 120,00</Text>
+            <Text style={ styles.incidentProperty }>VALOR:</Text>
+            <Text style={ styles.incidentValue }>R$ 120,00</Text>
 
-          <TouchableOpacity
-            style={ styles.detailsButton }
-            onPress={ () => {} }
-          >
-            <Text style={ styles.detailsButtonText }>Ver mais detalhes</Text>
-            <Feather name="arrow-right" size={ 16 } color="#e02041" />
-          </TouchableOpacity>
-        </View>
+            <TouchableOpacity
+              style={ styles.detailsButton }
+              onPress={ () => {} }
+            >
+              <Text style={ styles.detailsButtonText }>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={ 16 } color="#e02041" />
+            </TouchableOpacity>
 
-        <View style={ styles.incident }>
+          </View>
+        )}
+      />
 
-          <Text style={ styles.incidentProperty }>ONG:</Text>
-          <Text style={ styles.incidentValue }>APAD</Text>
-
-          <Text style={ styles.incidentProperty }>CASO:</Text>
-          <Text style={ styles.incidentValue }>Cadelinha atropelada</Text>
-
-          <Text style={ styles.incidentProperty }>VALOR:</Text>
-          <Text style={ styles.incidentValue }>R$ 120,00</Text>
-
-          <TouchableOpacity
-            style={ styles.detailsButton }
-            onPress={ () => {} }
-          >
-            <Text style={ styles.detailsButtonText }>Ver mais detalhes</Text>
-            <Feather name="arrow-right" size={ 16 } color="#e02041" />
-          </TouchableOpacity>
-        </View>
-
-        <View style={ styles.incident }>
-
-          <Text style={ styles.incidentProperty }>ONG:</Text>
-          <Text style={ styles.incidentValue }>APAD</Text>
-
-          <Text style={ styles.incidentProperty }>CASO:</Text>
-          <Text style={ styles.incidentValue }>Cadelinha atropelada</Text>
-
-          <Text style={ styles.incidentProperty }>VALOR:</Text>
-          <Text style={ styles.incidentValue }>R$ 120,00</Text>
-
-          <TouchableOpacity
-            style={ styles.detailsButton }
-            onPress={ () => {} }
-          >
-            <Text style={ styles.detailsButtonText }>Ver mais detalhes</Text>
-            <Feather name="arrow-right" size={ 16 } color="#e02041" />
-          </TouchableOpacity>
-        </View>
-      </View>
     </View>
   );
 }
