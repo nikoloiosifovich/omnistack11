00d3f627ef950ad529bc9d0387667409157d64be
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 22:37:34 -0300

    Implement: struct of NewIncident page

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,9 +1,37 @@
 import React from 'react' ;
 import { Link } from 'react-router-dom';
-import {} from 'react-icons';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg'
+
 export default function NewIncident() {
-  return <h1>NewIncident</h1>
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={ logoImg } alt="Logo: Be The Hero"/>
+
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para resolver isso.
+          </p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={ 16 } color="#e02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Título do caso"/>
+          <textarea placeholder="Descrição"/>
+          <input placeholder="Valor em reais"/>
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
