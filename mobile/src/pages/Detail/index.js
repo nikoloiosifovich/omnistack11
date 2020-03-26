6e2f8768852537186bf867bc0e2aa8e3fcbda5be
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 20:48:43 -0300

    Implement: whatsapp funcionality for Detail page

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Linking } from 'react-native';
 import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
@@ -10,7 +10,7 @@ import styles from './styles';
 
 export default function Detail() {
   const navigation = useNavigation();
-  const message = 'Olá APAD, estou entrando em contato pois gosria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00';
+  const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00.';
 
 
   function navigateGoBack() {
@@ -18,7 +18,7 @@ export default function Detail() {
   }
 
   function sendWhatsApp() {
-    navigation.navigate("");
+    Linking.openURL( `whatsapp://send?phone=5582998033096&text=${ message }` );
   }
 
   function sendEmail() {
@@ -57,7 +57,7 @@ export default function Detail() {
         <Text style={ styles.heroDescription }>Entre em contato:</Text>
 
         <View style={ styles.actions }>
-          <TouchableOpacity style={ styles.action } onPress={ () => {}}>
+          <TouchableOpacity style={ styles.action } onPress={ sendWhatsApp }>
             <Text style={ styles.actionText }>WhatsApp</Text>
           </TouchableOpacity>
 
