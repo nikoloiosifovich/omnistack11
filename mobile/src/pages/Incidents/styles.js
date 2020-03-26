e97e185eebb0614f22d9a34f59eac5f1926a3dec
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 18:40:22 -0300

    Implement: styles for header incidents page

---
+++ mobile/src/pages/Incidents/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  headerText: {
+    fontSize: 15,
+    color: '#737380'
+  },
+  headerTextBold: {
+    fontWeight: 'bold'
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+    marginTop: 48,
+    color: '#13131a',
+    fontWeight: 'bold'
+  },
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#737380'
+  },
+});
