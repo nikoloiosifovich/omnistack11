d80fc8cce15436b5b463c4bbbfaba2cd5b6125e6
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 00:20:34 -0300

    Implement: delete incidents func

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -26,6 +26,20 @@ export default function Profile() {
     } )
   }, [ ongId ] ) // se deixar vazio ele executa uma vez, com elemento ele executa quando esse mudar.
 
+  async function handleDeleteIncident( id ) {
+    try {
+      await api.delete( `incidents/${ id }`, {
+        headers: {
+          Authorization: ongId,
+        }
+      } );
+
+      setIncidents( incidents.filter( incident => incident.id !== id ) );
+    } catch (error) {
+      alert( 'Erro ao deletar caso, tente novamente!' );
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -54,7 +68,7 @@ export default function Profile() {
           <strong>VALOR:</strong>
           <p>{ Intl.NumberFormat( 'pt-Br', { style: 'currency', currency: 'BRL' }).format( incident.value ) }</p>
 
-          <button type="button">
+          <button onClick={ () => handleDeleteIncident( incident.id ) } type="button">
             <FiTrash2 size={ 20 } color="#a8a8b3" />
           </button>
           </li>
