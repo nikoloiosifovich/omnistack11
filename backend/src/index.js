6aa904bc66c61c133b3c84c845ceb684b31fc541
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-27 15:24:31 -0300

    Implement: errors status code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
-const express = require( 'express' );
-const cors = require( 'cors' );
-const routes = require( './routes' );
+const express = require("express");
+const cors = require("cors");
+const { errors } = require("celebrate");
+const routes = require("./routes");
 
 const app = express();
 
-app.use( cors() ); // { origin: 'url' }
-app.use( express.json() );
-app.use( routes );
+app.use(cors()); // { origin: 'url' }
+app.use(express.json());
+app.use(routes);
+app.use(errors());
 
-app.listen( 3333 );
+app.listen(3333);
